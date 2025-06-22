c78910bdd8bfa42ef3248226cde74f42291cf160
olamide-code <olamidewrite@gmail.com>
2025-06-22 21:06:39 +0100

    updated task

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 #indexing = accessing elements of a squence using [] (indexing opera)
 # [start : end : step]
 
+credit = "1234-5678-9012-3456"
+#print(credit[0])
+#print(credit[0:4])
+#print(credit[5:9])
+#print(credit[5:])
+
+last_digits = credit[-4:]
+print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+
+
